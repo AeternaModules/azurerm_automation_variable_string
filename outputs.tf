2879565e760116e6f9c9d75e15e2d230c0aabecf
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:57:02 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "automation_variable_strings" {
+  description = "All automation_variable_string resources"
+  value       = azurerm_automation_variable_string.automation_variable_strings
+}
+
