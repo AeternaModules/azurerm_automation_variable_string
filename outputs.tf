be24f4c5f0b6a2a7c8b2d277ba3c0fe0654f4590
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:11:22 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,28 @@ output "automation_variable_strings" {
   description = "All automation_variable_string resources"
   value       = azurerm_automation_variable_string.automation_variable_strings
 }
+output "automation_variable_strings_automation_account_name" {
+  description = "List of automation_account_name values across all automation_variable_strings"
+  value       = [for k, v in azurerm_automation_variable_string.automation_variable_strings : v.automation_account_name]
+}
+output "automation_variable_strings_description" {
+  description = "List of description values across all automation_variable_strings"
+  value       = [for k, v in azurerm_automation_variable_string.automation_variable_strings : v.description]
+}
+output "automation_variable_strings_encrypted" {
+  description = "List of encrypted values across all automation_variable_strings"
+  value       = [for k, v in azurerm_automation_variable_string.automation_variable_strings : v.encrypted]
+}
+output "automation_variable_strings_name" {
+  description = "List of name values across all automation_variable_strings"
+  value       = [for k, v in azurerm_automation_variable_string.automation_variable_strings : v.name]
+}
+output "automation_variable_strings_resource_group_name" {
+  description = "List of resource_group_name values across all automation_variable_strings"
+  value       = [for k, v in azurerm_automation_variable_string.automation_variable_strings : v.resource_group_name]
+}
+output "automation_variable_strings_value" {
+  description = "List of value values across all automation_variable_strings"
+  value       = [for k, v in azurerm_automation_variable_string.automation_variable_strings : v.value]
+}
 
