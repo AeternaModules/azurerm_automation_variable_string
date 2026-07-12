92a22287acce5b4ba9ece946ea0a998fb06774d4
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:05 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "automation_variable_strings" {
-  description = "All automation_variable_string resources"
-  value       = azurerm_automation_variable_string.automation_variable_strings
-}
 output "automation_variable_strings_automation_account_name" {
-  description = "List of automation_account_name values across all automation_variable_strings"
-  value       = [for k, v in azurerm_automation_variable_string.automation_variable_strings : v.automation_account_name]
+  description = "Map of automation_account_name values across all automation_variable_strings, keyed the same as var.automation_variable_strings"
+  value       = { for k, v in azurerm_automation_variable_string.automation_variable_strings : k => v.automation_account_name }
 }
 output "automation_variable_strings_description" {
-  description = "List of description values across all automation_variable_strings"
-  value       = [for k, v in azurerm_automation_variable_string.automation_variable_strings : v.description]
+  description = "Map of description values across all automation_variable_strings, keyed the same as var.automation_variable_strings"
+  value       = { for k, v in azurerm_automation_variable_string.automation_variable_strings : k => v.description }
 }
 output "automation_variable_strings_encrypted" {
-  description = "List of encrypted values across all automation_variable_strings"
-  value       = [for k, v in azurerm_automation_variable_string.automation_variable_strings : v.encrypted]
+  description = "Map of encrypted values across all automation_variable_strings, keyed the same as var.automation_variable_strings"
+  value       = { for k, v in azurerm_automation_variable_string.automation_variable_strings : k => v.encrypted }
 }
 output "automation_variable_strings_name" {
-  description = "List of name values across all automation_variable_strings"
-  value       = [for k, v in azurerm_automation_variable_string.automation_variable_strings : v.name]
+  description = "Map of name values across all automation_variable_strings, keyed the same as var.automation_variable_strings"
+  value       = { for k, v in azurerm_automation_variable_string.automation_variable_strings : k => v.name }
 }
 output "automation_variable_strings_resource_group_name" {
-  description = "List of resource_group_name values across all automation_variable_strings"
-  value       = [for k, v in azurerm_automation_variable_string.automation_variable_strings : v.resource_group_name]
+  description = "Map of resource_group_name values across all automation_variable_strings, keyed the same as var.automation_variable_strings"
+  value       = { for k, v in azurerm_automation_variable_string.automation_variable_strings : k => v.resource_group_name }
 }
 output "automation_variable_strings_value" {
-  description = "List of value values across all automation_variable_strings"
-  value       = [for k, v in azurerm_automation_variable_string.automation_variable_strings : v.value]
+  description = "Map of value values across all automation_variable_strings, keyed the same as var.automation_variable_strings"
+  value       = { for k, v in azurerm_automation_variable_string.automation_variable_strings : k => v.value }
 }
 
